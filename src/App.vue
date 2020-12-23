55c6741c44771231092d1504731225314451ee51
icanq <icanq@.localdomain>
2020-12-23 10:39:45 +0700

    add template already in component router, store, main

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/">Home</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <router-view></router-view>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss">
+  // Import Main styles for this application
+  @import 'assets/scss/style';
 </style>
